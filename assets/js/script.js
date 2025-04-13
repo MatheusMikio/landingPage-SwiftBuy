68e10d501da0fb93b8465d211579aa5df84d7990
Matheus Mikio <matheusmikio15@gmail.com>
2025-04-13 01:15:03 -0300

    Criação Rodape, menu mobile e alterações
    Criei o Rodape com as informações do Canva, o menu mobile quando reduzir a tela e alterei as cores de quase tudo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,26 @@
-window.addEventListener('scroll', () =>{
+window.addEventListener('scroll', () => {
     const barraSuperior = document.getElementById('barraSuperior');
-    if (window.scrollY > 100){
+    if (window.scrollY > 30) {
         barraSuperior.classList.add('fixed', 'top-0');
     }
-    else{
+    else {
         barraSuperior.classList.remove('fixed', 'top-0');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuBtn = document.getElementById('menuBtn');
+    const menuCelular = document.getElementById('menuCelular');
+    if (menuBtn && menuCelular) {
+        menuBtn.addEventListener('click', () => {
+            menuCelular.classList.toggle('hidden');
+        });
+
+        const links = menuCelular.querySelectorAll('a');
+        links.forEach(link => {
+            link.addEventListener('click', () => {
+                menuCelular.classList.add('hidden');
+            });
+        });
     }
 });
